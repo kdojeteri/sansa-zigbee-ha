5254812215440aa762b78051501000d27aabeb75
Teri Hornych <teri.hornych@notum.cz>
2024-04-05 15:38:34 +0200

    rename callbacks & pins

--- nrf52/uart-intercept/src/main.c
+++ nrf52/uart-intercept/src/main.c
@@ -8,17 +8,17 @@ LOG_MODULE_REGISTER(uart_intercept, LOG_LEVEL_INF);
 
 #define GPIOTE_INST 0
 #define GPIOTE_NODE DT_NODELABEL(_CONCAT(gpiote, GPIOTE_INST))
-#define RX_LOW_PIN 10
-#define RX_HIGH_PIN 9
+#define RX_FALL_PIN 10
+#define RX_RISE_PIN 9
 
-static struct gpio_dt_spec rx_low_dt = {.port = DEVICE_DT_GET(DT_NODELABEL(gpio0)), .pin = RX_LOW_PIN, .dt_flags = GPIO_PULL_DOWN};
-static struct gpio_dt_spec rx_high_dt = {.port = DEVICE_DT_GET(DT_NODELABEL(gpio0)), .pin = RX_HIGH_PIN};
+static struct gpio_dt_spec rx_fall_dt = {.port = DEVICE_DT_GET(DT_NODELABEL(gpio0)), .pin = RX_FALL_PIN, .dt_flags = GPIO_PULL_DOWN};
+static struct gpio_dt_spec rx_rise_dt = {.port = DEVICE_DT_GET(DT_NODELABEL(gpio0)), .pin = RX_RISE_PIN};
 static struct gpio_dt_spec tx_dt = {.port = DEVICE_DT_GET(DT_NODELABEL(gpio1)), .pin = 10};
-static struct gpio_callback rx_low_cb_data;
-static struct gpio_callback rx_high_cb_data;
+static struct gpio_callback rx_fall_cb_data;
+static struct gpio_callback rx_rise_cb_data;
 static uint32_t rx_k_cycles_prev = 0;
 
-void on_rx_high(const struct device *dev, struct gpio_callback *cb,
+void on_rx_rise(const struct device *dev, struct gpio_callback *cb,
                 uint32_t pins)
 {
         uint32_t rx_k_cycles = k_cycle_get_32();
@@ -31,7 +31,7 @@ void on_rx_high(const struct device *dev, struct gpio_callback *cb,
         }
 }
 
-void on_rx_low(const struct device *dev, struct gpio_callback *cb,
+void on_rx_fall(const struct device *dev, struct gpio_callback *cb,
                uint32_t pins)
 {
         uint32_t rx_k_cycles = k_cycle_get_32();
@@ -48,20 +48,20 @@ int main(void)
 {
         int ret;
 
-        if (!gpio_is_ready_dt(&rx_low_dt) || !gpio_is_ready_dt(&rx_high_dt) || !gpio_is_ready_dt(&tx_dt))
+        if (!gpio_is_ready_dt(&rx_fall_dt) || !gpio_is_ready_dt(&rx_rise_dt) || !gpio_is_ready_dt(&tx_dt))
         {
                 LOG_ERR("button device is not ready");
                 return 0;
         }
 
-        ret = gpio_pin_configure_dt(&rx_low_dt, GPIO_INPUT | GPIO_ACTIVE_LOW);
+        ret = gpio_pin_configure_dt(&rx_fall_dt, GPIO_INPUT | GPIO_ACTIVE_LOW);
         if (ret != 0)
         {
                 LOG_ERR("failed to configure pin: %d", ret);
                 return 0;
         }
 
-        ret = gpio_pin_configure_dt(&rx_high_dt, GPIO_INPUT | GPIO_ACTIVE_LOW);
+        ret = gpio_pin_configure_dt(&rx_rise_dt, GPIO_INPUT | GPIO_ACTIVE_LOW);
         if (ret != 0)
         {
                 LOG_ERR("failed to configure pin: %d", ret);
@@ -75,7 +75,7 @@ int main(void)
                 return 0;
         }
 
-        ret = gpio_pin_interrupt_configure_dt(&rx_low_dt,
+        ret = gpio_pin_interrupt_configure_dt(&rx_fall_dt,
                                               GPIO_INT_EDGE_TO_ACTIVE);
         if (ret != 0)
         {
@@ -83,7 +83,7 @@ int main(void)
                 return 0;
         }
 
-        ret = gpio_pin_interrupt_configure_dt(&rx_high_dt,
+        ret = gpio_pin_interrupt_configure_dt(&rx_rise_dt,
                                               GPIO_INT_EDGE_TO_INACTIVE);
         if (ret != 0)
         {
@@ -91,12 +91,12 @@ int main(void)
                 return 0;
         }
 
-        gpio_init_callback(&rx_low_cb_data, on_rx_low, BIT(rx_low_dt.pin));
-        gpio_init_callback(&rx_high_cb_data, on_rx_high, BIT(rx_high_dt.pin));
-        gpio_add_callback(rx_low_dt.port, &rx_low_cb_data);
-        gpio_add_callback(rx_high_dt.port, &rx_high_cb_data);
+        gpio_init_callback(&rx_fall_cb_data, on_rx_fall, BIT(rx_fall_dt.pin));
+        gpio_init_callback(&rx_rise_cb_data, on_rx_rise, BIT(rx_rise_dt.pin));
+        gpio_add_callback(rx_fall_dt.port, &rx_fall_cb_data);
+        gpio_add_callback(rx_rise_dt.port, &rx_rise_cb_data);
         rx_k_cycles_prev = k_cycle_get_32();
-        printk("Set up rx at %s low pin %d, high pin %d\n", rx_low_dt.port->name, rx_low_dt.pin, rx_high_dt.pin);
+        printk("Set up rx at %s low pin %d, high pin %d\n", rx_fall_dt.port->name, rx_fall_dt.pin, rx_rise_dt.pin);
 
         return 0;
 }
